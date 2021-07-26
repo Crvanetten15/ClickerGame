a060daf68a17f43bc46ad245171283ccdfc18e5e
Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>
2021-07-25 20:03:41 -0400

    Added files.

---
+++ src/me/connor/AppFrame.java
@@ -0,0 +1,18 @@
+package me.connor;
+
+import javax.swing.*;
+
+public class AppFrame extends JFrame {
+
+    // Constructors are like methods, but they have the exact name of the class and do not return anything, even void.
+    // They are used to initialize the class with any required information provided by usage.
+    // You can also do any "setting-up"
+
+    // There is no type parameter necessary for the constructor
+    public AppFrame(){
+
+        // You don't need to put anything in here, but you might want to set the window dimensions so that it isn't smushed...
+        setSize(600, 600);
+
+    }
+}
--- src/me/connor/Main.java
+++ src/me/connor/Main.java
@@ -1,8 +1,37 @@
 package me.connor;
+import java.util.*;
 
 public class Main {
 
+    /**
+     * I would encourage:
+     *  - Trying things on your own. Don't follow a video. Experiment!!
+     *  - Closely think about what each line does as you write code.
+     */
+
     public static void main(String[] args) {
-	// write your code here
+
+        // "new AppFrame();" returns an instance of AppFrame (the entire class)
+        // The "new" keyword calls the constructor of any non-static class.
+        AppFrame appFrame = new AppFrame();
+
+        // Make appFrame visible...
+        appFrame.setVisible(true);
+
+
+        // This variable is null, it has no definition, or "value."
+        // AppFrame iDeclaredThis;
+
+        // this variable is an instance of AppFrame
+        // AppFrame iInitializedThis;
+        // iInitializedThis = new AppFrame();
+
+        //                 |Type |Name|=|new|Type(<any parameters that Type requires)
+        // Same as this == Scanner in  = new Scanner(System.in);
+
+        // -----------------------------------------
+        // To quickly comment a selection out in IntelliJ, highlight a line(s) and press Ctrl + /
+        // Mac: Command + /
+
     }
 }
