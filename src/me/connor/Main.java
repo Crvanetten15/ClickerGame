0912907c3bbc3b2f9e3a11c111e76a7bfa3a1642
Crvanetten15 <crvanetten.ll@gmail.com>
2022-02-10 19:45:04 -0500

    Version 2.1.1

--- src/me/connor/Main.java
+++ src/me/connor/Main.java
@@ -3,31 +3,11 @@ import java.util.*;
 
 public class Main {
 
-    /**
-     * I would encourage:
-     *  - Trying things on your own. Don't follow a video. Experiment!!
-     *  - Closely think about what each line does as you write code.
-     */
-
     public static void main(String[] args) {
 
-        // "new AppFrame();" returns an instance of AppFrame (the entire class)
-        // The "new" keyword calls the constructor of any non-static class.
+
         new Details();
 
-        // This variable is null, it has no definition, or "value."
-        // AppFrame iDeclaredThis;
-
-        // this variable is an instance of AppFrame
-        // AppFrame iInitializedThis;
-        // iInitializedThis = new AppFrame();
-
-        //                 |Type |Name|=|new|Type(<any parameters that Type requires)
-        // Same as this == Scanner in  = new Scanner(System.in);
-
-        // -----------------------------------------
-        // To quickly comment a selection out in IntelliJ, highlight a line(s) and press Ctrl + /
-        // Mac: Command + /
 
     }
 }
